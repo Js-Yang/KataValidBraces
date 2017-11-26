a8f2f560f733f0e692155ecdb63cadd8ff1d430b
Js-Yang <chyang590@gmail.com>
2017-11-26 21:13:11 +0800

    add test case "input is (({{[[]]}})), should return true"

--- KataValidBraces/Brace.cs
+++ KataValidBraces/Brace.cs
@@ -7,7 +7,7 @@ public class Brace
         var brackets = new List<string>() { "[]", "{}", "()" };
         foreach (var bracket in brackets)
         {
-            if (((input.IndexOf(bracket[1]) - input.IndexOf(bracket[0])) + 1) % 2 == 1)
+            if ((input.IndexOf(bracket[1]) - input.LastIndexOf(bracket[0]) + 1) % 2 == 1)
             {
                 continue;
             }
@@ -22,18 +22,17 @@ public class Brace
 
     private static bool Contains(string bracket, string input)
     {
-        return input.IndexOf(bracket[0]) != -1 && input.IndexOf(bracket[1]) != -1;
+        return input.IndexOf(bracket[0]) != -1 && input.LastIndexOf(bracket[1]) != -1;
     }
 
     private static void Erase(string bracket, ref string input)
     {
-        foreach (var partOfBracket in bracket)
+        var start = bracket[0];
+        var end = bracket[1];
+        if (input.IndexOf(start) != -1)
         {
-            var index = input.IndexOf(partOfBracket);
-            if (index != -1)
-            {
-                input = input.Remove(index, 1);
-            }
+            input = input.Remove(input.IndexOf(start), 1);
+            input = input.Remove(input.LastIndexOf(end), 1);
         }
     }
 }
