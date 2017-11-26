6c61b76752471f9cb3161d5b4a6cfd88f0966786
Js-Yang <chyang590@gmail.com>
2017-11-26 20:57:22 +0800

    refactor: remove redundant parameter in test case

--- KataValidBraces.Tests/Brace.Test.cs
+++ KataValidBraces.Tests/Brace.Test.cs
@@ -5,21 +5,21 @@ namespace KataValidBraces.Tests
     [TestFixture]
     public class KataTest
     {
-        [TestCase("[]", true, TestName = "input is [], should return true")]
-        [TestCase("{[]}", true, TestName = "input is {[]}, should return true")]
-        [TestCase("{[()]}", true, TestName = "input is {[()]}, should return true")]
-        [TestCase("{[([])]}", true, TestName = "input is {[([])]}, should return true")]
-        public void validBraces_Input_Was_Valid_Should_Return_True(string input, bool expected)
+        [TestCase("[]", TestName = "input is [], should return true")]
+        [TestCase("{[]}", TestName = "input is {[]}, should return true")]
+        [TestCase("{[()]}", TestName = "input is {[()]}, should return true")]
+        [TestCase("{[([])]}", TestName = "input is {[([])]}, should return true")]
+        public void validBraces_Input_Was_Valid_Should_Return_True(string input)
         {
-            Assert.AreEqual(expected, Brace.validBraces(input));
+            Assert.AreEqual(true, Brace.validBraces(input));
         }
 
-        [TestCase("{]", false, TestName = "input is {], should return false")]
-        [TestCase("[{]", false, TestName = "input is {], should return false")]
-        [TestCase("[{]}", false, TestName = "input is [{]}, should return false")]
-        public void validBraces_Input_Was_Invalid_Should_Return_False(string input, bool expected)
+        [TestCase("{]", TestName = "input is {], should return false")]
+        [TestCase("[{]", TestName = "input is {], should return false")]
+        [TestCase("[{]}", TestName = "input is [{]}, should return false")]
+        public void validBraces_Input_Was_Invalid_Should_Return_False(string input)
         {
-            Assert.AreEqual(expected, Brace.validBraces(input));
+            Assert.AreEqual(false, Brace.validBraces(input));
         }
     }
 }
