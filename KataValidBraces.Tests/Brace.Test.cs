50e16df86eab059e96c6aa0c3ea6b112a6dfddc9
Js-Yang <chyang590@gmail.com>
2017-11-26 20:12:36 +0800

    add test case "input is {[([])]}, should return true"

--- KataValidBraces.Tests/Brace.Test.cs
+++ KataValidBraces.Tests/Brace.Test.cs
@@ -8,6 +8,7 @@ namespace KataValidBraces.Tests
         [TestCase("[]", true, TestName = "input is [], should return true")]
         [TestCase("{[]}", true, TestName = "input is {[]}, should return true")]
         [TestCase("{[()]}", true, TestName = "input is {[()]}, should return true")]
+        [TestCase("{[([])]}", true, TestName = "input is {[([])]}, should return true")]
         public void validBraces_Input_Was_Valid_Square_Should_Return_True(string input, bool expected)
         {
             Assert.AreEqual(expected, Brace.validBraces(input));
