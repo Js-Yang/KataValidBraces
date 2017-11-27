89b4c01a695a512220745d21f6f7db9d49a00048
Js-Yang <chyang590@gmail.com>
2017-11-27 23:47:25 +0800

    refactor: remove primitive obsession

--- KataValidBraces/Brace.cs
+++ KataValidBraces/Brace.cs
@@ -1,54 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Brace
 {
     public static bool validBraces(string input)
     {
-        Console.WriteLine(input);
-        var brackets = new List<string>() { "[]", "{}", "()" };
+        var brackets = new List<string>() { "[]", "{}", "()" }.Select(bracket => new Bracket(bracket)).ToList();
+
         foreach (var bracket in brackets)
         {
-            if (ValidDistinct(bracket, input) || ValidOrder(bracket, input))
+            if (bracket.IsValid(input))
             {
-                continue;
+                break;
             }
-            while (Contains(bracket, input))
-            {
-                Erase(bracket, ref input);
-            }
+
+            EraseBy(bracket, ref input);
         }
 
         return input == string.Empty;
     }
 
-    private static bool ValidOrder(string bracket, string input)
+    private static void EraseBy(Bracket bracket, ref string input)
     {
-        if (input.LastIndexOf(bracket[0]) == -1)
+        while (bracket.Contains(input))
+        {
+            bracket.Erase(ref input);
+        }
+    }
+}
+
+internal class Bracket
+{
+    private string bracket;
+
+    public char Open => bracket[0];
+
+    public char Close => bracket[1];
+
+    public Bracket(string bracket)
+    {
+        this.bracket = bracket;
+    }
+
+    public void Erase(ref string input)
+    {
+        input = input.Remove(input.LastIndexOf(Open), 1);
+        input = input.Remove(input.IndexOf(Close), 1);
+    }
+
+    public bool Contains(string input)
+    {
+        return input.LastIndexOf(Open) != -1 && input.IndexOf(Close) != -1;
+    }
+
+    public bool IsValid(string input)
+    {
+        if (!Contains(input))
         {
             return false;
         }
-        return (input.IndexOf(bracket[1], input.LastIndexOf(bracket[0])) < input.LastIndexOf(bracket[0]));
+
+        return ValidDistinct(input) || ValidOrder(input);
     }
 
-    private static bool ValidDistinct(string bracket, string input)
+    public bool ValidDistinct(string input)
     {
-        return (input.IndexOf(bracket[1]) - input.LastIndexOf(bracket[0]) + 1) % 2 == 1;
+        return (input.IndexOf(Close) - input.LastIndexOf(Open) + 1) % 2 == 1;
     }
 
-    private static bool Contains(string bracket, string input)
+    public bool ValidOrder(string input)
     {
-        return input.LastIndexOf(bracket[0]) != -1 && input.IndexOf(bracket[1]) != -1;
+        return (input.IndexOf(Close, input.LastIndexOf(Open)) < input.LastIndexOf(Open));
     }
 
-    private static void Erase(string bracket, ref string input)
-    {
-        var start = bracket[0];
-        var end = bracket[1];
-        if (input.IndexOf(start) != -1)
-        {
-            input = input.Remove(input.LastIndexOf(start), 1);
-            input = input.Remove(input.IndexOf(end), 1);
-        }
-    }
 }
